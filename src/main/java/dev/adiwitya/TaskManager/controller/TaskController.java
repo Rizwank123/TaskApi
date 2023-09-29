cb98a2482d07fee2bb56f58c99245e83e0e0fb91
Rizwank123 <expertkhan7@gmail.com>
2023-09-29 14:37:57 +0530

    swagger documentation completed

--- src/main/java/dev/adiwitya/TaskManager/controller/TaskController.java
+++ src/main/java/dev/adiwitya/TaskManager/controller/TaskController.java
@@ -18,38 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
 
 import dev.adiwitya.TaskManager.entity.Task;
 import dev.adiwitya.TaskManager.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 @RestController
 @RequestMapping("/api")
-//@Tag(name="Task Manager",description = "Task manager end points")
+@Tag(name="Task Manager",description = "Task manager end points")
 public class TaskController {
 	
 	@Autowired
 	private TaskService taskService;
-	
+	@Operation(summary = "creating new Taks")
 	@PostMapping("/tasks")
 	public ResponseEntity<Task> addTask(@RequestBody Task task){
 		
 		return new ResponseEntity<>(taskService.createTask(task),HttpStatus.CREATED);
 	}
+	@Operation(summary = "List of taks")
 	@GetMapping("/tasks")
 	public ResponseEntity<List<Task>> getTaskList(){
 		return ResponseEntity.ok(taskService.getAllTask());
 	}
+	@Operation(summary = "Delete task using task_id")
 	@DeleteMapping("/tasks/{taskId}")
 	public ResponseEntity<String> deleteTask(@PathVariable long taskId){
 		taskService.deleteTask(taskId);
 		return ResponseEntity.ok("Deleted Successfully");
 	}
+	@Operation(summary = "Update task using task_id")
 	@PutMapping("/task/{taskId}")
 	public ResponseEntity<Task>updateTask(@RequestBody Task task,@PathVariable long taskId){
 		return ResponseEntity.ok(taskService.updateTask(taskId, task));
 	}
+	@Operation(summary = "find task using task_id")
 	@GetMapping("/tasks/{taskId}")
 	public ResponseEntity<Task> getTaskById(@PathVariable long taskId){
 		return ResponseEntity.ok(taskService.getTaskById(taskId));
 	}
+	@Operation(summary = "change task status like completed,inprogress,pendding etc task using task_id")
 	@PatchMapping("/tasks/{taskId}")
 	public ResponseEntity<String> updateTaskStatus(@PathVariable long taskId,@RequestParam String status){
 		taskService.markAsCompleted(taskId, status);
--- src/main/java/dev/adiwitya/TaskManager/controller/UserController.java
+++ src/main/java/dev/adiwitya/TaskManager/controller/UserController.java
@@ -20,9 +20,12 @@ import dev.adiwitya.TaskManager.Security.JwtService;
 import dev.adiwitya.TaskManager.dto.LoginRequest;
 import dev.adiwitya.TaskManager.entity.UserInfo;
 import dev.adiwitya.TaskManager.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name="User Api end points")
 public class UserController {
 	@Autowired
 	private UserService userService;
@@ -35,18 +38,18 @@ public class UserController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
-	
+	@Operation(summary = "Method is use to register new User")
 	@PostMapping("/addUser")
 	public ResponseEntity<UserInfo> createUser(@RequestBody UserInfo user){
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		return new ResponseEntity<UserInfo>(userService.createUser(user),HttpStatus.CREATED);
 	}
-	
+	@Operation(summary = "Getting List of users ")
 	@GetMapping("/Users")
 	public ResponseEntity<List<UserInfo>> getAllUser(){
 		return ResponseEntity.ok(userService.getAllUser());
 	}
-	
+	@Operation(summary = "Token Generation end point")
 	@PostMapping("/getToken")
     public String authenticateAndGetToken(@RequestBody LoginRequest authRequest) {
         Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
