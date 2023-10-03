32ed5f7003f3a4a4fbcf6ad00c60f93558fc52ec
Rizwank123 <expertkhan7@gmail.com>
2023-10-03 20:36:38 +0530

    bug fix

--- src/main/java/dev/adiwitya/TaskManager/service/TaskServiceImpl.java
+++ src/main/java/dev/adiwitya/TaskManager/service/TaskServiceImpl.java
@@ -1,9 +1,14 @@
 package dev.adiwitya.TaskManager.service;
 
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import dev.adiwitya.TaskManager.Exception.ResourceNotFoundException;
 import dev.adiwitya.TaskManager.entity.Task;
@@ -16,6 +21,7 @@ public class TaskServiceImpl implements TaskService {
 	private TaskRepository taskRepo;
 	@Override
 	public Task createTask(Task task) {
+		task.setDueDate(timeFormater(task.getDueDate()));
 		
 		return taskRepo.save(task);
 	}
@@ -39,6 +45,14 @@ public class TaskServiceImpl implements TaskService {
 	public Task updateTask(long taskId, Task task)  {
 		Task tsk=taskRepo.findById(taskId);
 		if(tsk==null ) throw new ResourceNotFoundException("task Not found", "TaskId ", taskId);
+		if(task.getDescription()=="") tsk.setDescription(task.getDescription());
+		else tsk.setDescription(tsk.getDescription());
+		tsk.setDescription(task.getDescription());
+		tsk.setDueDate(timeFormater(task.getDueDate()));
+		if(task.getPriority().name()=="") tsk.setPriority(tsk.getPriority());
+		else tsk.setPriority(task.getPriority());
+		if(tsk.getName()=="") tsk.setName(tsk.getName());
+		else tsk.setName(task.getName());
 		
 		return taskRepo.update( tsk);
 	}
@@ -56,6 +70,24 @@ public class TaskServiceImpl implements TaskService {
 		if(tsk==null) throw new ResourceNotFoundException("task Not found", "taskId ", id);
 		tsk.setStatus(status);
 		taskRepo.updateStatus(tsk);
+	}
+	private static Timestamp timeFormater(Timestamp timestamp) {
+		 SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+	        // Format the Timestamp as a String
+	        String formattedTime = dateFormat.format(new Date(timestamp.getTime()));
+
+	        try {
+	            // Parse the formatted String back to a Date
+	            Date parsedDate = dateFormat.parse(formattedTime);
+
+	            // Create a new Timestamp from the parsed Date
+	            return new Timestamp(parsedDate.getTime());
+	        } catch (ParseException e) {
+	            // Handle parsing exceptions here
+	            e.printStackTrace();
+	            return null;
+	        }
 	}
 	
 
